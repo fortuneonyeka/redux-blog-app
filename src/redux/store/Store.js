9759d347d8014ff96005037b2e70466853210e86
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-25 13:18:26 +0100

    added postsReducer to the store

--- src/redux/store/Store.js
+++ src/redux/store/Store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../feature/posts/postsSlice"
 
 
 export const store = configureStore({
   reducer: {
-    
+    posts: postsReducer
   }
 })
